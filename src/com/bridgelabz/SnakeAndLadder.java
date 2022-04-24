0a31d422380edc8c3ea9e766b2b3b4c508ad38a9
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-24 09:12:01 -0400

    Added: dice roll logic is added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,14 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static final int START_POSITION = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake & Ladder game!");
-
+        System.out.println("Welcome to Snake & Ladder game! \nRoll a dice.");
+        Random random = new Random();
         int position = 0;
+        int diceNo = random.nextInt(6) + 1;
+        System.out.println("Dice value: "+diceNo);
     }
 }
