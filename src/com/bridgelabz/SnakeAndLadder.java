4209abe4886c1c95f4230a2bfa8bb00f7b263871
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-25 07:13:52 -0400

    Added: while loop added to repeat till player wins

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -7,25 +7,33 @@ public class SnakeAndLadder {
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake & Ladder game! \nRoll a dice.");
         Random random = new Random();
-        int position = 0;
-        int diceNo = random.nextInt(6) + 1;
-        System.out.println("Dice value: "+diceNo);
+        int position = 0, diceNo, options;
 
-        int options = random.nextInt(3);
-        switch (options){
-            case LADDER :
-                position += diceNo;
-                break;
-            case SNAKE:
-                if (position != 0)
-                    position -= diceNo;
-                break;
-            case NO_PLAY:
-                break;
+        while (position <= 100) {
+            diceNo = random.nextInt(6) + 1;
+            System.out.println("Dice value: " + diceNo);
+
+            options = random.nextInt(3);
+            switch (options) {
+                case LADDER:
+                    position += diceNo;
+                    break;
+                case SNAKE:
+                    if (position >= 0) {
+                        if (position - diceNo >= 0)
+                            position -= diceNo;
+                        else
+                            position = START_POSITION;
+                    }
+                    break;
+                case NO_PLAY:
+                    break;
+            }
+            System.out.println("Player is at position " + position);
         }
-        System.out.println("Player is at position "+position);
     }
 }
