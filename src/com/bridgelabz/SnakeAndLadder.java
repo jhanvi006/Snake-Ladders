30c7265310fe83b6d980de6df72259b7cb77656d
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-26 16:36:06 -0400

    Added: feature for two player game is added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -10,18 +10,33 @@ public class SnakeAndLadder {
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
 
-    public static int DiceRoll(){
+    public static int diceRoll(){
         int diceValue = random.nextInt(6) + 1;
         System.out.println("Dice value: " + diceValue);
         return diceValue;
     }
-    public static int PlayerMoveLadder(int diceNo, int position){
-        System.out.println("Yahoo! It's a ladder.");
-        if (position+diceNo<=END_POSITION)
-            position += diceNo;
-        return position;
+    public static int rollDiceForPlayer(int player, int playerPosition){
+        if (playerPosition == END_POSITION)
+            return playerPosition;
+        else {
+            int diceNo = diceRoll();
+            int options = random.nextInt(3);
+            playerPosition = calculatePlayerMovement(options, diceNo, playerPosition, player);
+            System.out.println("Player " + player + " is at position " + playerPosition);
+            return playerPosition;
+        }
     }
-    public static int PlayerMoveSnake(int diceNo, int position){
+    public static int playerMoveLadder(int diceNo, int position){
+        if (position == END_POSITION)
+            return position;
+        else {
+            System.out.println("Yahoo! It's a ladder.");
+            if (position + diceNo <= END_POSITION)
+                position += diceNo;
+            return position;
+        }
+    }
+    public static int playerMoveSnake(int diceNo, int position){
         System.out.println("Oops! It's a snake.");
         if (position >= 0) {
             if (position - diceNo >= 0)
@@ -31,13 +46,15 @@ public class SnakeAndLadder {
         }
         return position;
     }
-    public static int CalculatePlayerMovement(int options, int diceNo,int position){
+    public static int calculatePlayerMovement(int options, int diceNo,int position, int currentPlayer){
         switch (options) {
             case LADDER:
-                position = PlayerMoveLadder(diceNo, position);
+                position = playerMoveLadder(diceNo, position);
+                System.out.println("Player"+currentPlayer+" roll again.");
+                position = rollDiceForPlayer(currentPlayer, position);
                 break;
             case SNAKE:
-                position = PlayerMoveSnake(diceNo, position);
+                position = playerMoveSnake(diceNo, position);
                 break;
             case NO_PLAY:
                 System.out.println("Sorry, you can't move.");
@@ -46,25 +63,32 @@ public class SnakeAndLadder {
         return  position;
     }
     public static void main(String[] args) {
-
         System.out.println("Welcome to Snake & Ladder game! \nRoll a dice.");
-        int position = 0, diceNo, options, noOfDiceRolls = 0;
-//        int positionOfPlayer1 = 0, positionOfPlayer2 = 0;
+        int noOfDiceRolls1 = 0, noOfDiceRolls2=0;
+        int positionOfPlayer1 = 0, positionOfPlayer2 = 0;
+        /* Use boolean currentPlayer to know which player's turn is ongoing
+         * where true is for Player1 and
+         * false is for Player2 */
+        boolean currentPlayer = true;
 
-        while (position <= END_POSITION) {
-            if (position == END_POSITION){
-                System.out.println("Congratulations! You won.");
+        while (positionOfPlayer1 <= END_POSITION && positionOfPlayer2<=END_POSITION) {
+            if (positionOfPlayer1 == END_POSITION){
+                System.out.println("Congratulations! Player1 won.");
                 break;
-            }
-            else {
-                diceNo = DiceRoll();
-                noOfDiceRolls++;
-
-                options = random.nextInt(3);
-                position = CalculatePlayerMovement(options, diceNo, position);
-                System.out.println("Player is at position " + position);
+            } else if (positionOfPlayer2 == END_POSITION) {
+                System.out.println("Congratulations! Player2 won.");
+                break;
+            } else {
+                if (currentPlayer){
+                    positionOfPlayer1 = rollDiceForPlayer(1, positionOfPlayer1);
+                    currentPlayer = false;
+                }
+                else {
+                    positionOfPlayer2 = rollDiceForPlayer(2,positionOfPlayer2);
+                    currentPlayer = true;
+                }
             }
         }
-        System.out.println("The player rolls the dice "+noOfDiceRolls+" times to win.");
+//        System.out.println("The player rolls the dice "+noOfDiceRolls+" times to win.");
     }
 }
