b72ef8cc67db7715f9895fcec7f4e10ab77bb0b1
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-26 14:38:51 -0400

    Modified: Added static methods

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,41 +3,65 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class SnakeAndLadder {
+    static Random random = new Random();
     public static final int START_POSITION = 0;
+    public static final int END_POSITION = 100;
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake & Ladder game! \nRoll a dice.");
-        Random random = new Random();
-        int position = 0, diceNo, options, noOfDiceRolls = 0;
-
-        while (position <= 100) {
-            if (position == 100)
+    public static int DiceRoll(){
+        int diceValue = random.nextInt(6) + 1;
+        System.out.println("Dice value: " + diceValue);
+        return diceValue;
+    }
+    public static int PlayerMoveLadder(int diceNo, int position){
+        System.out.println("Yahoo! It's a ladder.");
+        if (position+diceNo<=END_POSITION)
+            position += diceNo;
+        return position;
+    }
+    public static int PlayerMoveSnake(int diceNo, int position){
+        System.out.println("Oops! It's a snake.");
+        if (position >= 0) {
+            if (position - diceNo >= 0)
+                position -= diceNo;
+            else
+                position = START_POSITION;
+        }
+        return position;
+    }
+    public static int CalculatePlayerMovement(int options, int diceNo,int position){
+        switch (options) {
+            case LADDER:
+                position = PlayerMoveLadder(diceNo, position);
                 break;
+            case SNAKE:
+                position = PlayerMoveSnake(diceNo, position);
+                break;
+            case NO_PLAY:
+                System.out.println("Sorry, you can't move.");
+                break;
+        }
+        return  position;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake & Ladder game! \nRoll a dice.");
+        int position = 0, diceNo, options, noOfDiceRolls = 0;
+//        int positionOfPlayer1 = 0, positionOfPlayer2 = 0;
+
+        while (position <= END_POSITION) {
+            if (position == END_POSITION){
+                System.out.println("Congratulations! You won.");
+                break;
+            }
             else {
-                diceNo = random.nextInt(6) + 1;
+                diceNo = DiceRoll();
                 noOfDiceRolls++;
-                System.out.println("Dice value: " + diceNo);
 
                 options = random.nextInt(3);
-                switch (options) {
-                    case LADDER:
-                        if (position+diceNo<=100)
-                            position += diceNo;
-                        break;
-                    case SNAKE:
-                        if (position >= 0) {
-                            if (position - diceNo >= 0)
-                                position -= diceNo;
-                            else
-                                position = START_POSITION;
-                        }
-                        break;
-                    case NO_PLAY:
-                        break;
-                }
+                position = CalculatePlayerMovement(options, diceNo, position);
                 System.out.println("Player is at position " + position);
             }
         }
